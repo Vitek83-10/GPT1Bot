e7021e24492ade26017a3483e159ed7e86051554
Vitek83-10 <vnedovesov83@gmail.com>
2025-06-27 09:36:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,15 @@ from pyrogram import Client, filters
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 
-app = Client("ViktorSignalBot", bot_token=BOT_TOKEN)
+app = Client(
+    "ViktorSignalBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
