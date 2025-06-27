370712c0d08a2a629777ca5149114f9c84f1e43a
Vitek83-10 <vnedovesov83@gmail.com>
2025-06-27 09:11:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,11 @@
-import os
 from pyrogram import Client, filters
+import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
 
 app = Client(
     "ViktorSignalBot",
-    bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH
+    bot_token=BOT_TOKEN
 )
 
 @app.on_message(filters.command("start"))
