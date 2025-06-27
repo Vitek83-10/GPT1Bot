d9f303317db8aa402675dcfa32815a56d7781bce
Vitek83-10 <vnedovesov83@gmail.com>
2025-06-27 07:12:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-app = Client("ViktorSignalBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("ViktorSignalBot", bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
