905bc0f0b1dea25b27ff2db7fea53a73c1145ca5
Vitek83-10 <vnedovesov83@gmail.com>
2025-07-01 10:40:59 +0200

    Update test_zentru.py

--- test_zentru.py
+++ test_zentru.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
+import os
 from telegram import Bot
 
-load_dotenv()  # Загружаем переменные из .env
-
+# Получаем токен и chat_id из переменных окружения
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("TARGET_CHAT_ID")
 
+# Создаём экземпляр бота и отправляем тестовое сообщение
 bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="✅ Zentru тест: бот работает через .env!")
+bot.send_message(chat_id=CHAT_ID, text="✅ Test passed! Бот работает.")
