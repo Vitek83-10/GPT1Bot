845ac60bd29e18e29d45f00e4277c4d71a347d02
Vitek83-10 <vnedovesov83@gmail.com>
2025-07-01 08:24:56 +0200

    Create test_send.py

---
+++ test_send.py
@@ -0,0 +1,17 @@
+import asyncio
+from pyrogram import Client
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+TARGET_CHAT_ID = int(os.environ.get("TARGET_CHAT_ID"))
+
+app = Client("test_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+async def send_test_message():
+    await app.start()
+    await app.send_message(chat_id=TARGET_CHAT_ID, text="✅ Это тестовое сообщение от бота. Связь установлена.")
+    await app.stop()
+
+asyncio.run(send_test_message())
