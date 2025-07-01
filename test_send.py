42ce81db66ac824b7b169c35949af9efd5767b58
Vitek83-10 <vnedovesov83@gmail.com>
2025-07-01 08:45:22 +0200

    Update test_send.py

--- test_send.py
+++ test_send.py
@@ -2,17 +2,17 @@ import asyncio
 from pyrogram import Client
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-TARGET_CHAT_ID = int(os.environ.get("TARGET_CHAT_ID"))  # например: 123456789
+API_ID = 20234202
+API_HASH = "fc0e099e810cbea903512acef8563b36"
+BOT_TOKEN = "8085881327:AAHtgjesSjMbyektB5W2YX1SDQAGk_MMPfc"
+TARGET_CHAT_ID = 1047298304  # ← твой настоящий Chat ID
 
 # Используем бот-сессию
 app = Client("test_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 async def send_test_message():
     await app.start()
-    await app.send_message(chat_id=TARGET_CHAT_ID, text="✅ Бот успешно отправил сообщение.")
+    await app.send_message(chat_id=TARGET_CHAT_ID, text="✅ Тестовое сообщение отправлено!")
     await app.stop()
 
 if __name__ == "__main__":
